b30ae2f1238fbaac6578132ac1969a4c58309632
Andrew Christie <awchristie86@gmail.com>
2022-01-07 21:14:13 -0500

    day4 pt2

--- day4/main.py
+++ day4/main.py
@@ -8,6 +8,8 @@ def main():
     numbers = lines[0].split(",")
     boards = lines[1:]
     boards_lst = []
+    continue_until_end = True
+    winner_count = 0
 
     for i in boards:
         new_lst = i.split(" ")
@@ -16,20 +18,27 @@ def main():
 
     lst = [boards_lst[i : i + 5] for i in range(0, len(boards_lst), 5)]
 
-    for n in numbers[:39]:
+    for n in numbers:
         lst = call_number(int(n), lst)
-        winning_board = check_winner(lst)
-        if winning_board:
-            print("Winning board!")
-            pprint(winning_board)
-            print("sum:", np.sum(winning_board))
-            print("number called:", n)
-            print("product:", np.sum(winning_board) * int(n))
-            break
+        winning_board_idx = check_winner(lst)
+        if winning_board_idx:
+            for wb in winning_board_idx:
+                winner_count += 1
+                winning_board = lst[wb]
+                print("Winning board!")
+                pprint(winning_board)
+                print("sum:", np.sum(winning_board))
+                print("number called:", n)
+                print("product:", np.sum(winning_board) * int(n))
+                print("boards won:", winner_count)
+                lst[wb] = np.zeros(25).reshape(5, 5) - 1
+                if continue_until_end:
+                    continue
+                else:
+                    break
 
 
 def call_number(number, boards):
-    print("Number is:", int(number))
     new_boards = []
     for b in boards:
         board = [[0 if number == j else j for j in i] for i in b]
@@ -38,10 +47,12 @@ def call_number(number, boards):
 
 
 def check_winner(boards):
-    for b in boards:
+    idx = []
+    for i, b in enumerate(boards):
         arr = np.array(b)
         if np.any(np.all(arr == 0, axis=0)) or np.any(np.all(arr == 0, axis=1)):
-            return b
+            idx.append(i)
+    return idx
 
 
 if __name__ == "__main__":
