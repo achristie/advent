8e73b2f5888232cc32ed512c2f81f0d3ddfd184b
Andrew Christie <awchristie86@gmail.com>
2022-01-08 22:45:31 -0500

    day 5 pt2

--- day5/main.py
+++ day5/main.py
@@ -1,6 +1,8 @@
 from pprint import pprint
 import numpy as np
 
+grid_size = (1000, 1000)
+
 
 def main():
     lines = [tuplize(line) for line in open("input.txt").read().splitlines()]
@@ -8,16 +10,20 @@ def main():
         line for line in lines if (line.is_horizontal() or line.is_vertical())
     ]
 
-    grid = np.zeros((1000, 1000))
+    grid = np.zeros(grid_size, int)
+    grid2 = np.zeros(grid_size, int)
     for l in only_straight:
         grid += l.get_grid()
 
-    pprint((grid >= 2).sum())
-    pprint(grid)
+    for l in lines:
+        grid2 += l.get_grid()
+
+    print("part 1:", (grid >= 2).sum())
+    print("part 2:", (grid2 >= 2).sum())
 
 
 class Line:
-    def_grid = np.zeros((1000, 1000))
+    def_grid = np.zeros(grid_size, int)
 
     def __init__(self, x1, x2, y1, y2):
         self.x1 = int(x1)
@@ -37,21 +43,32 @@ class Line:
     def is_vertical(self):
         return self.x1 == self.x2
 
+    def slope(self):
+        return (self.y2 - self.y1) / (self.x2 - self.x1)
+
     def get_grid(self):
         g = self.def_grid.copy()
 
         x_start = self.x1 if self.x1 < self.x2 else self.x2
-        x_end = abs(self.x2 - self.x1)
-        x_ones = np.ones(x_end + 1)
+        x_len = abs(self.x2 - self.x1) + 1
 
         y_start = self.y1 if self.y1 < self.y2 else self.y2
-        y_end = abs(self.y2 - self.y1)
-        y_ones = np.ones(y_end + 1)
+        y_len = abs(self.y2 - self.y1) + 1
 
         if self.is_horizontal():
-            g[y_start, x_start : x_start + x_end + 1] = x_ones
+            x_ones = np.ones(x_len)
+            g[y_start, x_start : x_start + x_len] = x_ones
+        elif self.is_vertical():
+            y_ones = np.ones(y_len)
+            g[y_start : y_start + y_len, x_start] = y_ones
         else:
-            g[y_start : y_start + y_end + 1, x_start] = y_ones
+            mtx = np.zeros(x_len * y_len).reshape(x_len, y_len)
+            if self.slope() > 0:
+                np.fill_diagonal(mtx, 1)
+            else:
+                np.fill_diagonal(np.fliplr(mtx), 1)
+
+            g[y_start : y_start + y_len, x_start : x_start + x_len] = mtx
 
         return g
 
