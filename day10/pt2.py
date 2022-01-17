fcf951f0f04e6002f4bb6dde362edd63a3695ba6
Andrew Christie <awchristie86@gmail.com>
2022-01-16 23:21:32 -0500

    finished pt 2

--- day10/pt2.py
+++ day10/pt2.py
@@ -1,6 +1,6 @@
-lines = [l for l in open("input_test.txt").read().splitlines()]
+lines = [l for l in open("input.txt").read().splitlines()]
 tags = {"[": "]", "(": ")", "<": ">", "{": "}"}
-points = {")": 1, "]": 2, "}": 3, ">": 4}
+points = {"(": 1, "[": 2, "{": 3, "<": 4}
 
 
 def find_opens(line):
@@ -15,7 +15,20 @@ def find_opens(line):
     return state
 
 
-for l in lines:
+def get_scores(line):
+    running_total = 0
     results = find_opens(l)
-    if results != corrupt:
-      out
+    if results != "corrupt":
+        for s in results[::-1]:
+            running_total = running_total * 5 + points[s]
+    return running_total
+
+
+scores = []
+for l in lines:
+    sc = get_scores(l)
+    if sc > 0:
+        scores.append(sc)
+
+print(sorted(scores))
+print("middle score:", sorted(scores)[len(scores) // 2])
