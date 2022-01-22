38e608c4ef0a6829c1a6aa7224780287113cfaa4
Andrew Christie <awchristie86@gmail.com>
2022-01-21 23:59:23 -0500

    day 15 pt1

--- day15/pt1.py
+++ day15/pt1.py
@@ -2,7 +2,7 @@ from pprint import pp
 from time import time
 import heapq
 
-grid = [[int(x) for x in l] for l in open("input_test.txt").read().splitlines()]
+grid = [[int(x) for x in l] for l in open("input.txt").read().splitlines()]
 x_size = len(grid)
 y_size = len(grid[0])
 
@@ -31,51 +31,19 @@ def find_adj(point):
 
 def find_path(grid):
     visited = [[0] * len(r) for r in grid]
-    paths = [(0, 0, 0)]  # total, y, x
-    pa = [(0, 0, 0)]
+    pq = [(0, 0, 0)]
 
     while True:
-        total, x, y = heapq.heappop(paths)  # Get coordinates for lowest path
-        print("hit")
+        total, x, y = heapq.heappop(pq)
         if (x, y) == (x_size - 1, y_size - 1):
-            return total
+            return (total, x, y)
         if visited[x][y]:
             continue
         visited[x][y] = 1
         for px, py in find_adj((x, y)):
-            print(paths[0])
-            if visited[x][y]:
+            if visited[px][py]:
                 continue
-            heapq.heappush(paths, (total + grid[px][py], px, py))
+            heapq.heappush(pq, (total + grid[px][py], px, py))
 
 
 print(find_path(grid))
-
-
-@timer
-def shortest_path(grid):
-    y_size, x_size = len(grid), len(grid[0])
-    paths = [(0, 0, 0)]  # total, y, x
-    visited = [[0] * len(row) for row in grid]
-    while True:
-        total, y, x = heapq.heappop(paths)  # Get coordinates for lowest path
-        if visited[y][x]:
-            continue
-        if (y, x) == (y_size - 1, x_size - 1):
-            return total
-        visited[y][x] = 1
-        for ny, nx in [
-            (y + 1, x),
-            (y, x + 1),
-            (y - 1, x),
-            (y, x - 1),
-        ]:  # prefer down and right
-            if not x_size > ny >= 0 <= nx < y_size:
-                continue
-            if visited[ny][nx]:
-                continue
-            # print(ny, nx)
-            heapq.heappush(paths, (total + grid[ny][nx], ny, nx))
-
-
-print(shortest_path(grid))
