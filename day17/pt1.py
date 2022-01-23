46ab897ce63ffc572f95b1f3d4dafd36e9ad092a
Andrew Christie <awchristie86@gmail.com>
2022-01-23 17:54:45 -0500

    day 17

--- day17/pt1.py
+++ day17/pt1.py
@@ -1,3 +1,6 @@
+from os import write
+
+
 target = open("input.txt").read().removeprefix("target area: ").split(",")
 x, y = [t.split("=")[1].split("..") for t in target]
 x = (int(x[0]), int(x[1]))
@@ -28,25 +31,39 @@ def within_area(area, position):
     return x_min <= x <= x_max and y_min <= y <= y_max
 
 
+def beyond_area(area, position):
+    x_min, x_max = area[0][0], area[0][1]
+    y_min, y_max = area[1][0], area[1][1]
+    x, y = position
+
+    return x > x_max or y < y_min
+
+
 def simulate(area):
     pos = (0, 0)
-    max_y = (0, 0, 0)  # height, dx, dy
-    for i in range(20):
-        for j in range(200):
+    intercepts = []  # height, dx, dy
+    for i in range(400):
+        for j in range(-500, 500):
             pos = (0, 0)
             y_prime = 0
             vi = (i, j)
             v = (i, j)
             counts = False
-            for _ in range(400):
+            while True:
                 pos, v = execute_step(v, pos)
                 y_prime = y_prime if pos[1] <= y_prime else pos[1]
                 if within_area(area, pos):
                     counts = True
-            if counts and max_y[0] <= y_prime:
-                max_y = (y_prime, vi[0], vi[1])
 
-    return max_y
+                if beyond_area(area, pos):
+                    break
+
+            if counts:
+                intercepts.append((y_prime, vi[0], vi[1]))
+
+    return intercepts
 
 
-print(simulate((x, y)))
+intercepts = simulate((x, y))
+print("Largest Intercept", sorted(intercepts, key=lambda x: x[0], reverse=True)[0])
+print("Count of intercepts", len(intercepts))
