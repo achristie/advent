3a798437290311f30ddb35924f7c47d352e02677
Andrew Christie <awchristie86@gmail.com>
2022-01-28 23:10:04 -0500

    day 16 again.

--- day16/pt1.py
+++ day16/pt1.py
@@ -1,12 +1,9 @@
-from string import hexdigits
-
-
-data = [format(int(h, 16), "040b") for h in open("input_test.txt").read().splitlines()]
+data = [bin(int("1" + h, 16))[3:] for h in open("input_test.txt").read().splitlines()]
 print(data)
 
 
-for h in data[2:3]:
-    h = h.lstrip("0")
+for h in data:
+    # h = h.lstrip("0")
     version = h[:3]
     type_id = h[3:6]
     num = ""
@@ -18,8 +15,9 @@ for h in data[2:3]:
                 num += h[x + 1 : x + 5]
     else:
         length_type_id = h[6]
-
-        if length_type_id == 0:
-
-
-    print(int(num, 2))
+        if length_type_id == "0":
+            length = int(h[7 : 7 + 15], 2)
+            print(h[7 : 7 + 15], length)
+        elif length_type_id == "1":
+            sub_packets = int(h[7 : 7 + 12], 2)
+            # skip 15 bits
